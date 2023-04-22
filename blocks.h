df4220afcc652e7a7acf1feece9836594295c1dd
Giggly Coffee <>
2023-04-23 00:01:05 +0530

    add upgrades dnf pkgs

--- blocks.h
+++ blocks.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
 	{"",    "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	        30,		 0    },
 	{"",     "~/.local/bin/statusbar/bar-bat",                              30,              0    }, 
 	{"",     "~/.local/bin/statusbar/bar-volume", 			       	 0,	 	10    },
 	{"",     "~/.local/bin/statusbar/bar-bright", 				 0, 		12    },
 	{"",     "date +'%a %d %b %I:%M'",					60,		 0    },
+	{" ",     "~/.local/bin/statusbar/upgrades",                            300,              0    },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
