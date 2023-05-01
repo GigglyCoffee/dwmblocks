9d35de18c6627c89792aea6a3238519c590f2a2c
Giggly Coffee <>
2023-05-01 09:16:30 +0530

    Change date format and sep

--- blocks.h
+++ blocks.h
@@ -3,12 +3,12 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
 	{"",    "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	        30,		 0    },
 	{"",     "~/.local/bin/statusbar/bar-bat",                              30,              0    }, 
-	{"",     "~/.local/bin/statusbar/bar-volume", 			       	 0,	 	10    },
+	{"",     "~/.local/bin/statusbar/bar-volume", 			       	60,	 	10    },
 	/*{"",     "~/.local/bin/statusbar/bar-bright", 				 0, 		12    },*/
-	{"",     "date +'%a %d %b %I:%M'",					60,		 0    },
+	{"",     "date +'%b %d[%w] %H:%M'",					60,		 0    },
 	{" ",     "~/.local/bin/statusbar/upgrades",                         3600,              0    },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  ";
+static char delim[] = "| ";
 static unsigned int delimLen = 5;
